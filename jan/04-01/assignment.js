f4ec3832f698ed1bfc7ef0807bdd643700e4800c
rubenSinzig <ruben.sinzig@posteo.de>
2021-01-04 11:38:39 +0100

    Assignment

---
+++ jan/04-01/assignment.js
@@ -0,0 +1,28 @@
+// 1. Declare a variable with the value of “I can walk in the park all day!“. Print the word “park” to the console.
+let inThePark = "I can walk in the park all day!"
+let shortInThePark = inThePark.substring(18, 22);
+console.log(shortInThePark);
+
+// 2. Declare a variable with the value of “Hello World”. Covert the value to upper case and print the converted value to the console.
+let world = "Hello World";
+let upWorld = world.toUpperCase();
+console.log(upWorld);
+
+// 3. Declare another variable with the value of “Earthlings”. Convert the value to lower case and print the converted value to the console.
+let person = "Earthlings";
+let lowPerson = person.toLowerCase();
+console.log(lowPerson);
+
+// 4. Declare a variable with the value “JavaScript”. Print the the characters “aSc” to the console using the substring() method.
+let java = "JavaScript";
+let subJava = java.substring(3, 6);
+console.log(subJava);
+
+// 5. Check if the sentence “nice shoes” contains the letter l.
+let shoe = "nice shoes";
+let checkShoe = shoe.includes("l");
+console.log(checkShoe);
+
+// 6. Create a new string from a given string with the first character of the given string added at the front and back. eg. of output: JavaScript => JJavaScriptJ.
+person = "EEarthlingsE";
+console.log(person);
