4519192934f8a68a388a8a3d01ba601e9aea8047
Hadi Nsreeny <hnsreeny@gmail.com>
2021-01-04 10:27:34 +0100

    string

---
+++ jan/04-01/index.js
@@ -0,0 +1,87 @@
+// printing out
+console.log("Hadi ");
+// var
+let x = 3;
+let str = "Berlin";
+let old = true;
+// ternary op
+let kid = 18;
+kid > 16
+  ? console.log("Yay I can Drive")
+  : console.log("No My Mom will drive me");
+// $ nodemon index.js
+// $ node index.js
+// typeof
+console.log(typeof x);
+console.log(typeof old);
+console.log(typeof str);
+// length
+// let firstName = "Hadi";
+// console.log(firstName.length);
+//console.log(firstName[0]);
+let something = 1231;
+something = 4567;
+console.log(something);
+const fatherName = "Wii";
+console.log(fatherName);
+var global = "Hi I am global";
+{
+  let local = 22;
+  global = "Hi";
+  var localGlob = "Hi It's me";
+}
+console.log(localGlob);
+print("Hadi");
+
+// function
+function print(xxx) {
+  console.log(xxx);
+}
+
+print("Hadi");
+print(22);
+console.log(lastName);
+var lastName = "Nsreeny";
+console.log(lastName);
+function sum(x, y) {
+  let z = 0;
+  z = x + y;
+  return z;
+}
+
+console.log(sum(2, 3));
+let sumRe = sum(2, 44);
+console.log(sumRe);
+
+// ----- option 1
+console.log(5);
+// ----- option 2
+let num = 5;
+console.log(num);
+// ----- option 3
+const a = 2;
+const b = 3;
+console.log(a + b);
+// ----- option 4
+console.log(3 - 2 - 4 + 2 + 6);
+// substring
+let oString = "Hi My Name is Hadi, I'm 31 y.";
+print(oString.length);
+let str1 = oString.substring(5);
+console.log(str1);
+let str2 = oString.substring(6, 10);
+console.log(str2);
+// toUpperCase
+let str3 = oString.toUpperCase();
+console.log(str3);
+// toLowerCase
+let str4 = oString.toLowerCase();
+console.log(str4);
+let firstName = "hAdI";
+// Hadi
+let firstChr = firstName[0].toUpperCase();
+console.log(firstChr);
+let restChr = firstName.substring(1).toLowerCase();
+console.log(restChr);
+let FinalFirstName = firstChr + restChr;
+console.log(FinalFirstName);
