ce1c6e26eab05d66f53e1c320556841da57e0749
rubenSinzig <ruben.sinzig@posteo.de>
2021-01-07 09:18:44 +0100

    rounding

--- jan/06-01/assignment.js
+++ jan/06-01/assignment.js
@@ -13,12 +13,12 @@ console.log(Math.max(-1, 0, 1, 2, 3, 4));
 // a. Round up
 // Round up each of the following numbers to the nearest integer: 3321.32321, 326.76, 76788.7, -9.78, 43.342.
 
-
+console.log(Math.ceil(3321.32321), Math.ceil(326.76), Math.ceil(76788.7), Math.round(-9.78), Math.ceil(43.342));
 
 // b. Round down
 // Round down each of the following numbers to the nearest integer: 3321.32321, 326.76, 76788.7, -9.78, 28.329.
 
-
+console.log(Math.floor(3321.32321), Math.floor(326.76), Math.floor(76788.7), Math.ceil(-9.78), Math.floor(28.329));
 
 // Strings and Numbers
 // 1. Concatenation.
