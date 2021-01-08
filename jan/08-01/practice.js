906148164bfa4595e152406860c55af783e90679
rubenSinzig <ruben.sinzig@posteo.de>
2021-01-08 16:49:07 +0100

    List with Array

---
+++ jan/08-01/practice.js
@@ -0,0 +1,18 @@
+// Arrays
+let list = [
+    "Paul",
+    "Anton",
+    "Max",
+    "Stefan",
+        [
+        45,
+         5,
+         84,
+         60,
+         99,
+        ],
+    "Laura",
+    "Sabine",
+    "Martina",
+]
+console.log(list);
