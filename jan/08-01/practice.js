ac59fe0f587d519bdce22072f9b8f2a3d2b66dbe
rubenSinzig <ruben.sinzig@posteo.de>
2021-01-08 18:25:10 +0100

    if and else

--- jan/08-01/practice.js
+++ jan/08-01/practice.js
@@ -16,3 +16,21 @@ let list = [
     "Martina",
 ]
 console.log(list);
+// Select elements from a Array
+console.log(list[2]);
+console.log(list[4][4]);
+// if
+let bool = "2" == 2;
+// if true ("Yes it's true") if false nothing happens
+if (bool) {
+    console.log("Yes it's true");
+}
+// if and else
+let visitors = 21;
+
+if (visitors < 20 ) {
+    console.log(`There are only ${visitors} visitors in the museum you can enter.`);
+}
+else {
+    console.log(`${visitors} are too much, sorry you can't enter the museum.`);
+}
