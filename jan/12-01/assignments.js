8395386e3e7a1169b2e9a7ca1e1ca6c54c461136
Hadi Nsreeny <hnsreeny@gmail.com>
2021-01-13 14:45:00 +0100

    founctions and arrays

--- jan/12-01/assignments.js
+++ jan/12-01/assignments.js
@@ -306,3 +306,44 @@ const passGen = () => {
 };
 
 console.log(passGen());
+
+// XO Create a function that takes a string, checks if it has the same number of 'x's and 'o's and returns either true or false.
+
+// Notes:
+
+// Return a boolean value (true or false).
+// The string can contain any character.
+// When neither an x nor an o is in the string, return true.
+// Examples:
+
+// isEqualNumXandO("ooxx") ➞ true
+// isEqualNumXandO("xooxx") ➞ false
+// isEqualNumXandO("ooxXm") ➞ true (case insensitive)
+// isEqualNumXandO("zpzpzpp") ➞ true (returns true if no x and o)
+// isEqualNumXandO("zzoo") ➞ false
+const isEqualNumXandO = (str) => {
+  let newStr = str.toLowerCase();
+  let counterX = 0;
+  let counterO = 0;
+  for (let i = 0; i < newStr.length; i++) {
+    if (newStr[i] == "x") {
+      counterX++;
+    }
+    // if (newStr[i] == "o") {
+    //   counterO++;
+    // }
+    if (newStr.includes("o")) {
+      counterO++;
+    }
+  }
+
+  if (counterX === counterO) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(isEqualNumXandO("zpzpzpp"));
+console.log(isEqualNumXandO("zzOo"));
+console.log(isEqualNumXandO("xxOo"));
