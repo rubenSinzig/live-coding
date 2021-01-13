729c5126b207345497619767c1f5b4de1d4f6d8b
rubenSinzig <ruben.sinzig@posteo.de>
2021-01-13 14:47:44 +0100

    assignment

--- jan/13-01/assignment.js
+++ jan/13-01/assignment.js
@@ -24,3 +24,44 @@ const countOccurrences = (str, letter) => {
 };
 
 console.log(countOccurrences("Now I try it by myself", "t")); // 
+
+
+// 11. XO Create a function that takes a string.
+// checks if it has the same number of ’x’s and ’o’s and returns either true or false.
+// Notes:
+// Return a boolean value (true or false).
+// The string can contain any character.
+// When neither an x nor an o is in the string, return true.
+// Examples:
+// isEqualNumXandO(“ooxx”) ➞ true
+// isEqualNumXandO(“xooxx”) ➞ false
+// isEqualNumXandO(“ooxXm”) ➞ true (case insensitive)
+// isEqualNumXandO(“zpzpzpp”) ➞ true (returns true if no x and o)
+// isEqualNumXandO(“zzoo”) ➞ false
+const isEqualNumXandO = (str) => {
+    let result;
+    let counterX = 0;
+    let counterO = 0;
+    let strArr = str.split(""); // string to Array
+    // console.log(strArr);
+    for (let i = 0; i < strArr.length; i++) {
+        // console.log(i);
+        if (strArr[i] == "x" || strArr[i] == "X") {
+            counterX++
+        }
+        if (strArr[i] == "o" || strArr[i] == "O") {
+            counterO++
+        }
+    }
+    if (counterX == counterO) {
+        return true;
+    }
+    else {
+        return false;
+    };
+}
+console.log(isEqualNumXandO("ooxx")); // true
+console.log(isEqualNumXandO("xooxx")); // false
+console.log(isEqualNumXandO("ooxXm")); // true (case insensitive)
+console.log(isEqualNumXandO("zpzpzpp")); // true (returns true if no x and o)
+console.log(isEqualNumXandO("zzoo")); // false
