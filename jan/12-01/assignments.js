43e6d8b9445e29e62c378b118327f03ff195c3cd
Hadi Nsreeny <hnsreeny@gmail.com>
2021-01-13 09:52:02 +0100

    look over functins, arrays and for loop

--- jan/12-01/assignments.js
+++ jan/12-01/assignments.js
@@ -1,5 +1,7 @@
-console.log("--------Exercise-------");
-// Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.
+// Create a function that takes a number.
+// as its only argument and returns true .
+//if it's less than or equal to zero,
+// otherwise return false.
 
 // Examples:
 
@@ -7,107 +9,52 @@ console.log("--------Exercise-------");
 // isLEQZero(0) ➞ true
 // isLEQZero(-4) ➞ true
 // isLEQZero(10) ➞ false
-console.log("--------Exercise-------");
+const isLEQZero = (num) => {
+  return num <= 0;
+};
+console.log(22 == 0);
 
-//  Dog Years. Create a function which calculates how old a dog is in dog years. The function should accept one argument that is the dog's age in human years.
-//PS:  calculate the dog's age in dog years based on the calculation of 1 human year = 7 dog years.
+console.log(isLEQZero(3)); //
 
-// Example:
-
-// dogAge(4) ➞ "Your doggy is 28 years old in dog years!"
-
-console.log("--------Exercise-------");
-
-//  A Lifetime Supply...
-// You just won a lifetime supply of your favorite snack! The snack company needs to calculate what that actually means and how much they need to give you over your "lifetime". Create a function to help you calculate that amount for yourself to make sure you get the right amount. The function should accept two arguments: person age and the amount he will eats per day. The function should calculate the amount that will be consumed for the rest of your life given a constant maximum age of 100.
-
-// Examples:
-
-// calcLifetimeSupply(25, 2) ➞ "The snack company should provide you with 54,788 units, until you are a ripe old age of 100. Happy snacking!"
-
-// calcLifetimeSupply(40, 3) ➞ "The snack company should provide you with 65,745 units, until you are a ripe old age of 100. Happy snacking!"
-
-console.log("--------Exercise-------");
 //  Number to Month Name
 // Create a function that takes a number (from 1 to 12) and returns its corresponding month name as a string.
 // For example, if you're given 3 as input, your function should return "March", because March is the 3rd month.
 //  here are the months array as a small help :)
-//   let months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 // Examples:
 
 // monthName(3) ➞ "March"
-// monthName(12) ➞ "December"
-// monthName(6) ➞ "June"
-
-console.log("--------Exercise-------");
-// Try to do it by yourself and don't look in yesterday solution. I trust you :)
-// Count Occurrences.
-// Create a function that accepts two arguments: a string and a letter. The function should count the number of occurrences of that letter in the string.
-
-// Example:
-
-// countOccurrences("this is a string", "i") ➞ 3
-
-console.log("--------Exercise-------");
-/*  draw this shapes in your console
-First shape
-T
-TT
-TTT
-TTTT
-
-Second shape
-****
-***
-**
-*
-
-Third shape
-1
-12
-123
-1234
-12345
-
-Bounce: Forth shape
-Be creative and show us your Artistic side
-
-
-*/
-
-console.log("--------Exercise-------");
-/*  Finish the following function so it outputs looks like this:
-1
-12
-123
-1234
-12345
-
-
- const numbersCount = () => {
-  for (let i = 0; i <= 5; i++) {
-    // something
-    for (let j = 1; j <= i; j++) {
-        // something
-    }
-      // something
+//
+const monthName = (monthNum) => {
+  let theNum = monthNum - 1;
+  let result = "";
+  if (theNum >= 0 && theNum <= 11) {
+    result = months[theNum];
+  } else {
+    result = `Sorry, you need to enter number between 1 and 12`;
   }
+  return result;
 };
+console.log(monthName(3000));
+console.log(monthName(1));
 
-numbersCount();
-
-
-Bonus: create another function by doing some changes to the previous function in order to have the following outputs :
-  12345
-  1234
-  123
-  12
-  1
-*/
-
-console.log("--------Exercise-------");
-/* create a function should that checks if a store contains a specific product in it's inventory.
+/* create a function should.
+ that checks if a store contains a specific product.
+  in it's inventory.
 for example:
 let storeArr= ["milk", "eggs", "cheese", "butter"];
 // try includes might help :)
@@ -117,15 +64,33 @@ let storeArr= ["milk", "eggs", "cheese", "butter"];
 inventoryCheck("cheese") returns true
 inventoryCheck( "ham") returns false 
 */
+let storeArr = ["milk", "eggs", "cheese", "butter"];
+const inventoryCheck = (str) => {
+  return storeArr.includes(str);
+};
 
-console.log("--------Exercise-------");
-// Extra work :)
-// create a repo for this app. we are coming later to it :)
-// Create a program that generate a random password with the length of 30 character, every time you call the function should give you a different password
+console.log(inventoryCheck("ham"));
+console.log(inventoryCheck("cheese"));
 
-// Examples you will have other passwords for sure
+//  A Lifetime Supply...
+// You just won a lifetime supply of your favorite snack! .
+//The snack company needs to calculate what that actually means and how much they need to give you over your "lifetime".
+// Create a function to help you calculate that amount for yourself to make sure you get the right amount. The function should accept two arguments:
+// person age and the amount he will eats per day.
+// The function should calculate the amount that will be consumed for the rest of your life given a constant maximum age of 100.
 
-// passwordGen()  ➞ Y3mI9x7bvPY86T?oM§hIbgh!MLwA77N
-// passwordGen()  ➞ Oy$tkBGoo5fg§mO4AdV&Kh8XXZR4d7K
-// passwordGen()  ➞ v0ZwDrn@hfI€kOKWH§k6kqub6zfpuyP
-console.log("--------Exercise-------");
+// Examples:
+
+// calcLifetimeSupply(25, 2) ➞ "The snack company should provide you with 54,788 units, until you are a ripe old age of 100. Happy snacking!"
+
+// calcLifetimeSupply(40, 3) ➞ "The snack company should provide you with 65,745 units, until you are a ripe old age of 100. Happy snacking!"
+
+const calcLifetimeSupply = (age, amountPerDay) => {
+  // let result = 0;
+  let amountPerYear = amountPerDay * 365;
+  let yearsToLive = 100 - age;
+  let result = amountPerYear * yearsToLive;
+
+  return `The snack company should provide you with ${result} units, until you are a ripe old age of 100. Happy snacking!`;
+};
+console.log(calcLifetimeSupply(40, 3));
