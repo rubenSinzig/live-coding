965d91fe9210742302b14a5ffce5a5c378bb6ea0
Hadi Nsreeny <hnsreeny@gmail.com>
2021-01-18 11:38:12 +0100

    For loop

--- jan/18-01/index.js
+++ jan/18-01/index.js
@@ -46,11 +46,99 @@ for (let i = 0; i <= 10; ) {
 // for (let i = 0; i <= 10; i++) {
 //   console.log(i % 2 == 0 ? `${i} is even` : "");
 // }
+/* 
+ +
+ ++
+ +++
+ ++++
+ ++++
+ +++
+ ++
+ +
+
+ 1
+ 12
+ 123
+ 1234
+
+*/
+let drawShape = "";
+for (let i = 0; i <= 4; i++) {
+  drawShape = "";
+  for (let j = 0; j <= i; j++) {
+    // +++
+    drawShape += "+";
+  }
+  console.log(drawShape);
+  //
+}
+
+//console.log(`  333 \n 34444 `);
+const names = ["Zain", "Olga", "Nancy"];
+const workOrHa = ["WebDev", "Sleep", "Eat"];
+for (let i = 0; i < names.length; i++) {
+  console.log(`hi I am ${names[i]}, I will be ${workOrHa[i]} `);
+}
 
 const images = ["😎", "🤓", "🙄"];
 const likes = [22, 344, 666];
-console.log(`
-this image 
-got likes
+for (let i = 0; i < images.length; i++) {
+  console.log(`
+   ____________
+  | this img ${images[i]}
+  |   got ${likes[i]}
+  l____________
+  `);
+}
 
-`);
+// Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / (height * height). (mass in kg, i.e. 72.5, and height in metres, i.e. 1.65).
+
+// Store Mark's and John's mass and height in variables.
+const markH = 1.65;
+const markM = 85;
+const johnH = 1.9;
+const johnM = 120;
+// Calculate both their BMIs and store their BMIs in variables.
+let markBMI = markM / (markH * markH);
+let johnBMI = johnM / (johnH * johnH);
+// console.log(markBMI);
+// Create a boolean variable containing information about whether Mark has a higher BMI than John.
+let bool = markBMI > johnBMI;
+console.log(typeof bool);
+// Print a string to the console containing the variable from step 3 using string interpolation. (e.g. " [boolean], it is").
+console.log(`Is Mark's BMI higher than John's? Why yes, it's ${bool}`);
+// Create an if statement which prints the name and BMI of the person with the highest BMI.
+
+if (markBMI > johnBMI) {
+  console.log("Mark is the winner");
+} else {
+  console.log("John is the winner");
+}
+
+// Make a variable for firstName and age and give each variable values.
+// Create an if/else statement to check whether the person’s age is less than 13.
+// If so, print “firstName is a child”.
+// If the age is equal to or more than 13 and less than 20,
+// print “firstName is a teenager”.
+// If the person’s age is equal to and more than 20 and less than 30,
+// then print “firstName is a young adult”.
+// If none of these conditions apply, print “firstName is a adult”.
+
+const lifeStatus = (firstName, age) => {
+  let status = "";
+  if (age < 13) {
+    status = "child";
+  } else if (age >= 13 && age < 20) {
+    status = "teenager";
+  } else if (age >= 20 && age < 30) {
+    status = " young adult";
+  } else {
+    status = "adult";
+  }
+  return `${firstName} is a ${status}`;
+};
+let firstName = "Hadi";
+let age = 31;
+
+console.log(lifeStatus(firstName, age));
+console.log(lifeStatus("Zain", 12));
