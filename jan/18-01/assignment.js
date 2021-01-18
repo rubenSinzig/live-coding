68932fb7ce326f452c2ca053787d53a67236ba89
rubenSinzig <ruben.sinzig@posteo.de>
2021-01-18 11:32:03 +0100

    improvements assignment BMI

--- jan/18-01/assignment.js
+++ jan/18-01/assignment.js
@@ -25,16 +25,16 @@ console.log("---------------");
 // Create a boolean variable containing information about whether Mark has a higher BMI than John.
 // Print a string to the console containing the variable from step 3 using string interpolation. (e.g. “Is Mark’s BMI higher than John’s? Why yes, it’s [boolean], it is”).
 // Create an if statement which prints the name and BMI of the person with the highest BMI.
-let markMass = 78.5;
-let markHeight = 1.82;
-let johnMass = 65;
-let johnHeight = 1.68;
-const bmi = (mass, height) => {
+const bmi = () => {
   // result = "";
+  let markMass = 78.5;
+  let markHeight = 1.82;
+  let johnMass = 65;
+  let johnHeight = 1.68;
   markBMI = markMass / (markHeight * markHeight);
   johnBMI = johnMass / (johnHeight * johnHeight);
   let bool = markBMI > johnBMI;
-  console.log(`Is Mark’s BMI higher than John’s? Why yes, it’s ${bool}, it is`);
+  console.log(`Is Mark’s BMI higher than John’s? Yes it’s ${bool}.`);
   if (markBMI > johnBMI) {
     console.log(`Mark has an BMI of ${Math.round(markBMI)}.`);
   }
@@ -43,5 +43,12 @@ const bmi = (mass, height) => {
   }
   // return result;
 };
-bmi(markMass, markHeight);
-bmi(johnMass, johnHeight);
+bmi();
+// More conditions
+// Make a variable for firstName and age and give each variable values.
+// Create an if/else statement to check whether the person’s age is less than 13.
+// If so, print “firstName is a child”.
+// If the age is equal to or more than 13 and less than 20, print “firstName is a teenager”.
+// If the person’s age is equal to and more than 20 and less than 30,
+// then print “firstName is a young adult”.
+// If none of these conditions apply, print “firstName is a adult”.
