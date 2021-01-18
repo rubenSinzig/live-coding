5a4f476a44a87f16ec4121293d76017df56e7d4c
rubenSinzig <ruben.sinzig@posteo.de>
2021-01-18 09:55:02 +0100

    live-coding 18-01

---
+++ jan/18-01/assignment.js
@@ -0,0 +1,18 @@
+// Loops
+for (let i = 0; i < 5; i++) {
+  console.log("print out 5 times");
+}
+console.log("---------------");
+let numb = 10;
+for (let i = 0; i <= numb; i++) {
+  if (i <= 1) {
+    console.log(`There is ${i} star in the sky.`);
+  } else {
+    console.log(`There are ${i} star's in the sky.`);
+  }
+}
+console.log("---------------");
+for (let i = 10; i > 0; i--) {
+  console.log(`i is now ${i}`);
+}
+console.log("---------------");
