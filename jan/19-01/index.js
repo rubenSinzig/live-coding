83db47ca3542c2565034b1a16cc23a7292127e4c
Hadi Nsreeny <hnsreeny@gmail.com>
2021-01-19 10:04:39 +0100

    Arrays ...

--- jan/19-01/index.js
+++ jan/19-01/index.js
@@ -2,33 +2,73 @@ const names = ["Zain", "Nancy", "Olga"];
 const smileys = ["😎", "🤓", "🥳"];
 // .length
 // to get the number of elements in an array
-
+console.log(names.length);
 // .length -1
 // to access the last item
+console.log(names[names.length - 1]);
 
-// .join()
-// create a string from an array
+let cityName = [];
+console.log(cityName.length);
+cityName[0] = "Paris";
+cityName[1] = "Berlin";
+cityName[2] = "Dubai";
 
-// .split()
-// divide a string into an array
+console.log(cityName);
 
 // .push()
 // add an item
-
+cityName.push("Hi");
+cityName.push("Hi");
+cityName.push("Cool");
+console.log(cityName);
 // .pop()
 // remove an item
+cityName.pop();
+cityName.pop();
+let removedItem = cityName.pop();
 
-// .shift()
-// add an item to the beginning
+console.log(cityName);
+console.log(removedItem);
 
 // .unshift()
+// add an item to the beginning
+cityName.unshift("🍀");
+console.log(cityName);
+//  .shift()
 // remove an item to the beginning
-
+let removeEle = cityName.shift();
+console.log(cityName);
+console.log(removeEle);
+//const names = ["Zain", "Nancy", "Olga"];
 // .indexOf()
 // Find the index of an item
-
-// .splice()
-// remove an item by the index number
-
+console.log(names.indexOf("Nancy"));
+let pos = names.indexOf("Nancy");
+// console.log(typeof pos);
+let n = 0;
+// // .splice()
+// // remove an item by the index number
+let removedItemFromIndex = names.splice(pos, n);
+console.log(names);
+console.log(removedItemFromIndex);
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
+let str = "Hi this is me 😎";
+// .split()
+// divide a string into an array
+let newArr = str.split(" ");
+console.log(newArr);
+// .join()
+// create a string from an array
+let newStr = newArr.join(" ");
+console.log(newStr);
 // .slice()
 // to make a shallow copy
+
+let arr = [];
+
+for (let i = 0; i < 5; i++) {
+  arr.push(i);
+}
+console.log(arr);
