70c2a13bba8d8ee6a9a0e32d7071e08b0913967a
rubenSinzig <ruben.sinzig@posteo.de>
2021-01-19 14:11:06 +0100

    assignment no.1

--- jan/19-01/assignment.js
+++ jan/19-01/assignment.js
@@ -54,5 +54,67 @@ findGreatest([3, 4, 5], 4);
 findGreatest([10, 20, 30], 12);
 findGreatest([0, 10, 3], 4);
 console.log("---------------------");
-//
+// AEIOU:
+// Vowels.
+// Create a function that takes a string in its parameters and counts the number of vowels
+// (i.e. in English, “a, e, i, o, u”) in the string.
+// i.e. findVowels(“this is a string”) ➞ 4
+const findVowels = (str) => {
+  let counter = 0;
+  let strToArr = str.split("");
+  // console.log(strToArr);
+  for (let i = 0; i < strToArr.length; i++) {
+    if (strToArr[i].includes("a")) {
+      counter++;
+    } else if (strToArr[i].includes("e")) {
+      counter++;
+    } else if (strToArr[i].includes("i")) {
+      counter++;
+    } else if (strToArr[i].includes("o")) {
+      counter++;
+    } else if (strToArr[i].includes("u")) {
+      counter++;
+    }
+  }
+  return counter;
+};
+console.log(findVowels("this is a string"));
+// not my solution
+// const findVowels = (str) => {
+//     let counter = 0;
+//     const vowels = ['a','e','i','o','u'];
+//     let strToArr = str.split("");
+//     console.log(strToArr);
+//     for (let i = 0; i < strToArr.length; i++) {
+//       for (let j = 0; j < vowels.length; j++) {
+//         if (strToArr[i] === vowels[j]) {
+//           counter++;
+//         }
+//       }
+//     }
+//     return counter;
+//   };
+//   console.log(findVowels("thiis is a string"));
+// 1 step   i = 0 j = 0
+// 2 step   i = 0 j = 1
+// 3 step   i = 0 j = 2
+// 4 step   i = 0 j = 3
+// 5 step   i = 0 j = 4
+// 6 step   i = 0 j = 5
+// 7 step   i = 1 j = 0
+// 8 step   i = 1 j = 1
+// .........i = 1 j = 5
+//.......... i = 15 j = 5 --> end of loops
+// WRONG
+// let strOne = "a, e, i, o, u";
+// let strTwo = "this is a string";
+// let strThree = "a, a, a, c";
+// const findVowels = (str) => {
+//   let strToArr = str.split(" ");
+//   // console.log(strToArr);
+//   return strToArr.length;
+// };
+// console.log(`a, e, i, o, u -> ${findVowels(strOne)}`);
+// console.log(`this is a string -> ${findVowels(strTwo)}`);
+// console.log(`a, a, a, c -> ${findVowels(strThree)}`);
 console.log("---------------------");
