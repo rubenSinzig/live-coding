63299013801f8cf52135bf0a11757279297e5906
rubenSinzig <ruben.sinzig@posteo.de>
2021-01-19 15:46:08 +0100

    assignment no.3

--- jan/19-01/assignment.js
+++ jan/19-01/assignment.js
@@ -144,5 +144,27 @@ console.log(collection([1, 6, 6, 9, 9]));
 console.log(collection([2, 2, 2, 2, 2, 2]));
 console.log(collection([5, 10, 15, 20, 25]));
 console.log("---------------------");
-//
+// Dictionary.
+// (take your time on this pls ) also as tip you can use .includes()
+// Create a function that takes an initial string and an array of words,
+// and returns a filtered array of the words that start with the same letters as the initial string.
+//  Notes:
+//  If none of the words match, return an empty array.
+// Keep the filtered array in the same relative order as the original array of words.
+// Examples:
+// dictionary(“bu”, [“button”, “breakfast”, “border”]) ➞ [“button”]
+// dictionary(“tri”, [“triplet”, “tries”, “trip”, “piano”, “tree”]) ➞ [“triplet”, “tries”, trip”]
+// dictionary(“beau”, [“pastry”, “delicious”, “name”, “boring”]) ➞ []
+const dictionary = (str, arr) => {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].includes(str)) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+};
+console.log(dictionary("bu", ["button", "breakfast", "border"]));
+console.log(dictionary("tri", ["triplet", "tries", "trip", "piano", "tree"]));
+console.log(dictionary("beau", ["pastry", "delicious", "name", "boring"]));
 console.log("---------------------");
