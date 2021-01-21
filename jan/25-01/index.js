8e63fbb0aa62d1a9c62feda13246c9b26e4e7cd0
Hadi Nsreeny <hnsreeny@gmail.com>
2021-01-21 11:23:09 +0100

    More small apps solutions

--- jan/25-01/index.js
+++ jan/25-01/index.js
@@ -5,6 +5,9 @@
 // unique([0, 0, 0.77, 0, 0]) ➞ 0.77
 // unique([0, 1, 1, 1, 1, 1, 1, 1]) ➞ 0
 // unique([3, 3, 3, 7, 3, 3]) ➞ 7
+
+const unique = () => {};
+
 // The Greater Numbers Create a function which accepts two arguments: the first argument being an array of numbers, and the second argument being a number. The function should return the elements of the array which are greater than the second argument.
 
 // Examples:
@@ -12,18 +15,24 @@
 // findGreatest([3, 4, 5], 4) ➞ 5
 // findGreatest([10, 20, 30], 12) ➞ 20, 30
 // findGreatest([0, 10, 3], 4) ➞ 10
+
+const findGreatest = () => {};
+
 // The longest word Create a function to find the longest word in a given string.
 
 // Example: longestWord("this is a web development course") ➞ "development"
 
+const longWord = () => {};
 // Reverse Create a function to reverse a number.
 
 // Example: reverse(34532) ➞ 23543
+const reverse = () => {};
 
 // Alphabetical Order Create a function to sort a string into alphabetical order. NB: assume numbers, symbols and punctuation are not included in the string.
 
 // Example: alphaOrder("webdev") ➞ "bdeevw"
 
+const alphaOrder = () => {};
 // Bonus Questions
 
 // c4n y0u r34d th15? Create a function that takes a string as an argument and returns a coded (h4ck3r 5p34k) version of the string. NB: for your program to work properly, the function should replace all 'a's with 4, 'e's with 3, 'i's with 1, 'o's with 0, and 's's with 5.
@@ -33,12 +42,17 @@
 // hackerSpeak("javascript is cool") ➞ "j4v45cr1pt 15 c00l"
 // hackerSpeak("programming is fun") ➞ "pr0gr4mm1ng 15 fun"
 // hackerSpeak("become a coder") ➞ "b3c0m3 4 c0d3r"
+
+const hackerSpeak = () => {};
+
 // snake_case ➞ camelCase Create a function toCamelCase() that takes a single string in snake_case and converts it into camelCase.
 
 // Examples:
 
 // toCamelCase("hello_world") ➞ "helloWorld"
 // toCamelCase("javascript_is_fun") ➞ "javaScriptIsFun"
+
+const toCamelCase = () => {};
 // Is it Symmetrical? Create a function that takes a number as an argument and returns true or false depending on whether the number is symmetrical or not. NB: A number is symmetrical when it is the same as its reverse.
 
 // Examples:
@@ -48,6 +62,8 @@
 // isSymmetrical(44444444) ➞ true
 // isSymmetrical(9939) ➞ false
 // isSymmetrical(1112111) ➞ true
+
+const isSymmetrical = () => {};
 // Pig Latin Translation Create a function that takes a string of words and moves the first letter of each word to the end of it, then adds 'ay' to the end of the word. This is a basic form of "Pig Latin". Move the first letter of each word to the end of the word. Add "ay" to the end of the word. Words starting with a vowel (A, E, I, O, U) append "way" to the end instead. Extra Practice
 
 // Preserve proper capitalization as in the examples below. Examples:
@@ -55,3 +71,5 @@
 // pigLatin("Cats are great pets.") ➞ "Atscay areway reatgay etspay."
 // pigLatin("Tom got a small piece of pie.") ➞ "Omtay otgay away allsmay iecepay ofway iepay."
 // // pigLatin("He told us a very exciting tale.") ➞ "Ehay oldtay usway away eryvay excitingway aletay."
+
+const pigLatin = () => {};
