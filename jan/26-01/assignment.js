025b5f750bbe9fa8b307f53779685af5859cbd08
rubenSinzig <ruben.sinzig@posteo.de>
2021-01-26 10:45:09 +0100

    assignment Four

--- jan/26-01/assignment.js
+++ jan/26-01/assignment.js
@@ -56,6 +56,16 @@ console.log("-------- assignment Four --------");
 // console.log(sum(1)) ---> 1
 // console.log(sum(1, 15)) ---> 16
 // console.log(sum(25, 25, 20)) ---> 70
+const sum = (...num) => {
+  let result = 0;
+  for (let i = 0; i < num.length; i++) {
+    result += num[i];
+  }
+  return result;
+};
+console.log(sum(1));
+console.log(sum(1, 15));
+console.log(sum(25, 25, 20));
 console.log("-------- assignment Five --------");
 // Bonus: Write a function which accepts any amount of numbers and returns the average.
 // e.g
