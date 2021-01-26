9002474d5f437548329343ee0df3db2ea247f344
Hadi Nsreeny <hnsreeny@gmail.com>
2021-01-26 09:01:05 +0100

    Functions params && Array methods

---
+++ jan/26-01/index.js
@@ -0,0 +1,21 @@
+// parameters: Naming and order
+
+// default value
+
+// Spread syntax ...
+
+// ...array
+
+// ...args
+
+// cool functions
+
+// .map
+// Converting an array to a different array: `Array.prototype.map(<function>)`
+
+// .filter
+// - Getting an array with items that aline with your condition: `Array.prototype.filter(<function>)`
+
+// .find
+// - Getting a the first item that aline with your condition : `Array.prototype.filter(<function>)`
+//  Maybe more 😏
