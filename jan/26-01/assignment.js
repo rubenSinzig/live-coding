ac1398d99b15c78561540a13c2e84543acc2175b
rubenSinzig <ruben.sinzig@posteo.de>
2021-01-26 14:54:10 +0100

    hackerSpeak

--- jan/26-01/assignment.js
+++ jan/26-01/assignment.js
@@ -1,22 +1,42 @@
 // const userPrint = (Parameters or Arguments) => {}
 //
+//
 // const halloUser = (userName = "default value") => {
 //  console.log(`Hi ${userName}.`)
 // }
 // halloUser();
 // halloUser("Paul");
 //
+//
 // const arr = ["Paul", "Sabine", "Mike", "Martina"];
 // const arrClone = [...arr]; // Clone the arr
+//
 //
 // const printThings = (userName = "default Name", age, ...args) => { // ...args creat a Array with the user input
 //  console.log(`Hi ${userName} I am ${age}, you like ${args}.`)
 // };
 // printThings("Paul", 31, "Coding", "Sport", "Drawing", "Do fun things");
 //
+//
 // (function welcomeFunction() {
 //   console.log(`Welcome back.`)
 // })();
+//
+//
+// .map(item, index)
+// const books = ["Book One", "Book Two", "Book Three"];
+// const printBook = books.map((item, index) => `${index}. title ${item}`);
+//
+//
+// .reduce(acc, cur)
+//
+//
+// .filter(item)
+//
+//
+// .find()
+//
+//
 console.log("-------- assignment One --------");
 // Write a function that it returns  “Two for me and one for you” when no arguments are passed
 // e.g
@@ -84,4 +104,51 @@ console.log(average(0));
 console.log(average(1, 2));
 console.log(average(1, 3, 6, 10));
 console.log(average(12, 14, 16));
+console.log("-------- hackerSpeak --------");
+// c4n y0u r34d th15? Create a function that takes a string as an argument
+// and returns a coded (h4ck3r 5p34k) version of the string.
+// NB: for your program to work properly, the function should replace
+// all 'a's with 4, 'e's with 3, 'i's with 1, 'o's with 0, and 's's with 5.
+// Examples:
+// hackerSpeak("javascript is cool") ➞ "j4v45cr1pt 15 c00l"
+// hackerSpeak("programming is fun") ➞ "pr0gr4mm1ng 15 fun"
+// hackerSpeak("become a coder") ➞ "b3c0m3 4 c0d3r"
+const hackerSpeak = (str) => {
+  let result = [];
+  const strToArr = str.toLowerCase().split("");
+  // console.log(strToArr);
+  const hackerLanguage = strToArr.map((item, index) => {
+    switch (item) {
+      case "a":
+        result.push(4);
+        break;
+      case "e":
+        result.push(3);
+        break;
+      case "i":
+        result.push(1);
+        break;
+      case "o":
+        result.push(0);
+        break;
+      case "s":
+        result.push(5);
+        break;
+      default:
+        result.push(item);
+    }
+  });
+  return result.join("");
+};
+console.log(hackerSpeak("javascript is cool"));
+console.log(hackerSpeak("programming is fun"));
+console.log(hackerSpeak("become a coder"));
+console.log("-------- Odds and Evens --------");
+// Odds and Evens.
+// Create a program that changes a given array by adding 1 to each odd integer and subtracting 1 from each even integer. Examples:
+// [3, 5, 2, 4] ➞ expected output: [4, 6, 1, 3]
+// [6, 9, 10, 20] ➞ expected output: [5, 10, 9, 19]
+console.log("-------- how many chr in a str --------");
+// how many chr in a str(not case sens)
+// howManyStr(“I like pizza”,“p”) -> 1
 console.log("-------- assignment End --------");
