1f0a413c19be8647b178968a993265a5b3f59314
Hadi Nsreeny <hnsreeny@gmail.com>
2021-02-02 15:23:03 +0100

    Objects

--- feb/02-02/index.js
+++ feb/02-02/index.js
@@ -64,12 +64,13 @@ const person = {
     );
   },
   old: function () {
-    let currentYear = new Date().getFullYear();
+    let currentYear = new Date().getFullYear(); // 2021
     return currentYear - this.birthYear;
   },
 };
 
 person.print();
+
 console.log(person.old());
 // Object.keys
 let keysArr = Object.keys(person);
@@ -80,11 +81,12 @@ console.log(Object.values(person));
 
 // Object.assign
 const obj3 = { kids: 2, cars: 400 };
-const obj4 = { colors: ["Red", "Gray", "Black"], pet: "fish" };
+const obj4 = { colors: ["Red", "Grey", "Black"], pet: "fish" };
 const totalObjects = Object.assign({ name: "Hadi" }, obj3, obj4);
 console.log(totalObjects);
 // Object.defineProperty()
-
+console.log(obj4["colors"][1]);
+console.log(obj4.colors[1]);
 const newPersonObject = {};
 Object.defineProperty(newPersonObject, "bike", {
   value: 22,
@@ -182,3 +184,126 @@ console.log(getKeysAndValues(objectToArray));
 
 delete objectToArray.cats;
 console.log(objectToArray);
+console.log("----------------");
+// Check if a number is within a given range. Write a program that checks if a number is within the range of an object's min and max properties. Assume min <= max is always true.
+
+// Examples:
+
+// (4, { min: 0, max: 5 }) ➞ true
+
+// (4, { min: 4, max: 5 }) ➞ true
+
+// (4, { min: 6, max: 10 }) ➞ false
+
+// (5, { min: 5, max: 5 }) ➞ true
+
+function isWithInTheRange(num, obj) {
+  return num >= obj.min && num <= obj.max;
+}
+console.log(isWithInTheRange(4, { min: 0, max: 5 }));
+console.log("----------------");
+
+// Scrabble. Write a program that, given an array of scrabble tiles, counts the maximum score that a player can earn from the tiles in their hand.
+// Example:
+// [ { tile: "N", score: 1 }, { tile: "K", score: 5 }, { tile: "Z", score: 10 }, { tile: "X", score: 8 }, { tile: "D", score: 2 }, { tile: "A", score: 1 }, { tile: "E", score: 1 } ]
+const scrabble = [
+  { tile: "N", score: 1 },
+  { tile: "K", score: 5 },
+  { tile: "Z", score: 10 },
+  { tile: "X", score: 8 },
+  { tile: "D", score: 2 },
+  { tile: "A", score: 1 },
+  { tile: "E", score: 1 },
+];
+
+// The player's maximum score: 1 + 5 + 10 + 8 + 2 + 1 + 1 = 28
+
+function sumTotalScrabble(arr) {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i]["score"]; // 1  5   10  8  2  1  1 = 28
+  }
+  return total;
+}
+// other way soon
+console.log("----------------");
+console.log(sumTotalScrabble(scrabble));
+// Is it an empty object? Write a program that returns true if an object is empty, and false if otherwise.
+
+// Examples:
+
+// {} ➞ true
+
+// {a: 1} ➞ false
+
+function isEmpty(obj) {
+  return Object.keys(obj).length == 0;
+}
+console.log(isEmpty({}));
+console.log(isEmpty({ a: 1 }));
+console.log("----------------");
+
+// Counting Letters. Create a function that counts the number of occurrences of each letter in a string. Return an object with key pair values of letters and the number of occurrences for each letter.
+
+// Example:
+
+// countLetters("tree") ➞ {t: 1, r: 1, e: 2}
+
+// Free Shipping. Create a function that determines whether an online order should get free shipping. An order gets free shipping if the total cost of items exceeds €50.
+
+// Examples:
+
+// freeShipping({ "Sponge": 3.50, "Soap": 5.99 }) ➞ false
+
+// freeShipping({ "Surround Sound Equipment": 499.99 }) ➞ true
+
+// freeShipping({ "Wool": 13.99, "Knitting Needles": 15.50, "Bag": 13.99 }) ➞ false
+
+console.log("----------------");
+
+// Programming Object.
+
+// const programming = {
+
+// languages: ["JavaScript", "Python", "Ruby"],
+
+// isChallenging: true,
+
+// isRewarding: true,
+
+// difficulty: 8,
+
+// jokes:
+
+// "http://stackoverflow.com/questions/234075/what-is-your-best-programmer-joke"
+// };
+
+// Write the command to add the language "Go" to the end of the languages array.
+
+// Change the difficulty to the value of 7.
+
+// Using the delete keyword, write the command to remove the jokes key from the programming object.
+
+// Write a command to add a new key called isFun and a value of true to the programming object.
+
+// Using a loop, iterate through the languages array and console.log all of the languages.
+
+// Using a loop, console.log all of the keys in the programming object.
+
+// Using a loop, console.log all of the values in the programming object.
+
+// Create an object method where if the keys "isChallenging" and "isRewarding" have values of "true", then return "Learning the programming languages: "JavaScript, Python, Ruby, Go" is rewarding and challenging. Bonus: In a comment, explain what is printed if we console.log an object method without calling it and why.
+
+// Bonus:
+
+// Make sure that any other code cannot delete or change properties of the object.
+console.log("----------------");
+
+// Create a function that returns an object has following output.
+
+// Examples:
+
+// ABC -> { A: 1, B: 1, C: 1 } QQQ -> { Q: 3}
+
+function countLettersAdvancedVersion(str) {}
+console.log(countLettersAdvancedVersion("tree"));
