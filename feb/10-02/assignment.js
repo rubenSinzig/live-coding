9fb72775fc6aafec0aebccde0c688a9d0ab30434
rubenSinzig <ruben.sinzig@posteo.de>
2021-02-10 11:10:31 +0100

    Return Keys and Values

--- feb/10-02/assignment.js
+++ feb/10-02/assignment.js
@@ -24,3 +24,37 @@ console.log(
     { tile: "E", score: 1 },
   ])
 );
+// 4. Switch Keys and Values.
+// Create a function to get a copy of an object.
+// The copy must switch the keys and values.
+// Example:
+// let person = {
+//   name: “John”,
+//   job: “teacher”
+// }
+// Expected Output:
+// {“John”: name, “teacher”: job}
+let person = {
+  name: "John",
+  job: "teacher",
+};
+const switchKeyValue = () => {
+  let copyPerson = { ...person };
+  // NOT FINISH !!!
+};
+console.log(switchKeyValue());
+// 5. Return Keys and Values.
+// Write a program that takes an object and returns an array which
+// contains two arrays:
+// one for the keys of the object and the other for the values of the object.
+// Examples:
+// { a: 1, b: 2, c: 3 } ➞ [[“a”, “b”, “c”], [1, 2, 3]]
+// {key: true} ➞ [[“key”], [true]]
+const keysAndValues = (obj) => {
+  let keysArr = Object.keys(obj);
+  let valuesArr = Object.values(obj);
+  let result = [keysArr, valuesArr];
+  return result;
+};
+console.log(keysAndValues({ a: 1, b: 2, c: 3 }));
+console.log(keysAndValues({ key: true }));
