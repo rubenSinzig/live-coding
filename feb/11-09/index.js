3b39d2bb53f1a5fc7fc7f830e80c80554f3ec456
Hadi Nsreeny <hnsreeny@gmail.com>
2021-02-11 10:36:45 +0100

    Constructor

--- feb/11-09/index.js
+++ feb/11-09/index.js
@@ -33,7 +33,7 @@ userData.fullName; // getting the object data
 userData.fullName = "Hadi Nsreeny 31 true"; // filling the object
 userData.fullName; // getting the new data
 
-// constructor
+// constructor own
 function Person(name, age, act) {
   this.name = name;
   this.age = age;
@@ -48,10 +48,30 @@ let zain = new Person("Zain", 21, ["sleep", "eat"]);
 zain.printOut();
 const olga = new Person("Olga", 40, "No thing");
 olga.printOut();
-// isDivisible function explained
 
 // for in  object
+for (let prop in zain) {
+  console.log(`zain.${prop} : ${zain[prop]}`);
+}
 
+let family = [
+  ["Zain", 22, "teacher"],
+  ["Olga", 40, "chef"],
+];
+for (let i = 0; i < family.length; i++) {
+  for (let j = 0; j < family[i].length; j++) {
+    console.log(family[i][j]);
+  }
+}
 // forEach  arr
-
+family.forEach((arr) => arr.forEach((element) => console.log(element)));
 // for of  arr
+for (let [key, val] of Object.entries(zain)) {
+  console.log(` ${key} : ${val}`);
+}
+
+// Creat a constructor function that receive product details (productName,prise,inventory) thin build a method to display this data in clear outputted form
+// build two product using your constructor function
+// i.e : (Pepsi,2.33,2232) -> Form item Pepsi selling prise 2.33€. we have 2232 in our store
+
+// get & set, create a car object that uses getter and setter method to get and set the following ()
