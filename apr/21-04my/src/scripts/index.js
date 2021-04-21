78bfdba14b73b1804a83707f332b5869f9996868
rubenSinzig <ruben.sinzig@posteo.de>
2021-04-21 16:39:34 +0200

    html -> add content for task & js -> add function for task

--- apr/21-04my/src/scripts/index.js
+++ apr/21-04my/src/scripts/index.js
@@ -41,3 +41,37 @@ function minus() {
 
   document.querySelector(".resultMinus").innerHTML = resultMin;
 }
+
+// function for counting the number of vowels
+// It doesn't work try to find a solution later
+function count() {
+  let userInput = document.querySelector("#vowels").value;
+  let strToArr = document
+    .querySelector("#vowels")
+    .value.toLowerCase()
+    .split("");
+  for (let i = 0; i <= strToArr.length; i++) {
+    let result = 0;
+    switch (strToArr[i]) {
+      case "a":
+        result += 1;
+        continue;
+      case "e":
+        result += 1;
+        continue;
+      case "i":
+        result += 1;
+        continue;
+      case "o":
+        result += 1;
+        continue;
+      case "u":
+        result += 1;
+        continue;
+      default:
+        break;
+    }
+  }
+  let output = `${userInput}: ${result}`;
+  document.querySelector(".resultCounting").innerHTML = output;
+}
