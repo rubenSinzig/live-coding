8d505c45888a7e03fb29d1fd68baf44af098847d
hnsreeny <hnsreeny@gmail.com>
2021-04-26 14:18:10 +0200

    ToDo App

--- apr/26-04/index.js
+++ apr/26-04/index.js
@@ -13,16 +13,21 @@ const create = () => {
   document.body.appendChild(newElement);
 };
 
-const addToList = () => {
+const addToList = (e) => {
+  // Preventing the from sending information out aka Reloading the page
+  e.preventDefault();
   let userData = document.querySelector("#userData").value;
   console.log(userData);
+  // Checking my input if it has a text
   if (userData != "") {
     let newLi = document.createElement("li");
     let text = document.createTextNode(userData);
     newLi.appendChild(text);
+    // Adding random colour to my list item
     newLi.style.color = colorGen();
     document.querySelector(".result").appendChild(newLi);
     document.querySelector("#userData").value = "";
+    // Other way to add random colours for all of the list items that are in my page
     // const listItems = document.querySelectorAll("li");
     // listItems.forEach((item) => {
     //   item.style.color = colorGen();
@@ -30,16 +35,30 @@ const addToList = () => {
     const newContainer = document.createElement("div");
     const doneButton = document.createElement("button");
     const deleteButton = document.createElement("button");
+    // const checkBox = document.createElement("input");
+    // checkBox.type = "checkbox";
+    // newContainer.appendChild(checkBox);
+    // Adding some text
     doneButton.innerHTML = "✓";
     deleteButton.innerHTML = "✗";
+    // Injecting the buttons into the container
     newContainer.appendChild(doneButton);
     newContainer.appendChild(deleteButton);
+    // Adding class
+    doneButton.classList.add("done");
     newLi.appendChild(newContainer);
+    // Complete function
+    // toggle will check if the class name exist, will remove it and if it's not will add it
+    const check = () => newLi.classList.toggle("completed");
+    // const del = () => newLi.remove() ;
+    // Adding event listener to my buttons
+    deleteButton.addEventListener("click", () => newLi.remove());
+    doneButton.addEventListener("click", check);
   } else {
     document.querySelector("#userData").placeholder = "Please Enter text first";
   }
 };
-
+// Random colour generator in hexa number
 const colorGen = () => {
   let result = "#";
   let colorCode = "0123456789ABCDEF";
@@ -48,3 +67,14 @@ const colorGen = () => {
   }
   return result;
 };
+// Functional check which key the user is pressing
+const keyCheck = (event) => {
+  console.log(event);
+  if (e.key == "Enter") addToList();
+};
+// Adding event listener to the input
+// const userInput = document.querySelector("#userData");
+// userInput.addEventListener("keypress", keyCheck);
+
+// Adding event listener to the form
+document.querySelector("form").addEventListener("submit", addToList);
