4690613212835d377951ad2bf56ce6956df47a83
hnsreeny <hnsreeny@gmail.com>
2021-04-26 09:20:50 +0200

    DOM Children 👼

---
+++ apr/25.04/index.js
@@ -0,0 +1,11 @@
+const create = () => {
+  // 1. step
+  const newElement = document.createElement("div");
+
+  const newText = document.createTextNode("I am a new element 😉");
+
+  // 2. step
+  // div                  // text
+  newElement.appendChild(newText);
+  document.body.appendChild(newElement);
+};
