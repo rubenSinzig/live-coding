daa9cc98083e7856cb69c6962e37f32cc3295396
hnsreeny <hnsreeny@gmail.com>
2021-04-26 09:44:48 +0200

    DOM Children 👼

--- apr/25.04/index.js
+++ apr/25.04/index.js
@@ -9,3 +9,20 @@ const create = () => {
   newElement.appendChild(newText);
   document.body.appendChild(newElement);
 };
+
+const addToList = () => {
+  let userData = document.querySelector("#userData").value;
+  //   console.log(userData);
+  if (userData != "") {
+    let newLi = document.createElement("li");
+    let text = document.createTextNode(userData);
+    newLi.appendChild(text);
+    document.querySelector(".result").appendChild(newLi);
+    document.querySelector("#userData").value = "";
+  } else {
+    document.querySelector("#userData").placeholder = "Please Enter text first";
+  }
+};
+
+// document.write("Hi I am text");
+//document.write("<h1> Hi h1>");
