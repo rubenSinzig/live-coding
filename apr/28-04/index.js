62e28e70c25b6713d1cbb67c556c46c9bd3e3d4a
hnsreeny <hnsreeny@gmail.com>
2021-04-28 10:01:29 +0200

    DOM Traversing

--- apr/28-04/index.js
+++ apr/28-04/index.js
@@ -8,17 +8,6 @@ console.log(ele2);
 // firstChild
 const mainText = document.querySelector("#text").firstElementChild;
 console.log(mainText.textContent);
-// nodeName, nodeType, id, innerText
-let text = document.querySelector("span");
-let closetEle = text.closest(".cool");
-console.log(closetEle);
-
-let names = document.querySelectorAll("li");
-names.forEach((name) => {
-  console.log(name);
-  if (name.matches(".teacher"))
-    console.log(`${name.textContent} is a teacher for FBW 48-1`);
-});
 
 // textContent gets the content of all elements, including <script> and <style> elements. In contrast.
 // innerText only shows “human-readable” elements.
@@ -30,9 +19,23 @@ names.forEach((name) => {
 
 // more on parentNode
 //https://developer.mozilla.org/en-US/docs/Web/API/ParentNode
-
+// nodeName, nodeType, id, innerText
 // closest
-
+let text = document.querySelector("span");
+let closetEle = text.closest(".cool");
+console.log(closetEle);
 // matches
-
+let names = document.querySelectorAll("li");
+names.forEach((name) => {
+  console.log(name.nodeType);
+  if (name.matches(".teacher"))
+    console.log(`${name.textContent} is a teacher for FBW 48-1`);
+});
 // querySelector
+const allChildren = document.querySelector("body").children;
+console.log(allChildren);
+let menuList = document.getElementById("menu");
+console.log(menuList.firstElementChild);
+console.log(menuList.lastElementChild);
+
+// all comments in your html page should be printed in console
