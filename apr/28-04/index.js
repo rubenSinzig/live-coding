06451084b7b0520e0fbc98af8513c2f1729fa3bb
hnsreeny <hnsreeny@gmail.com>
2021-04-28 09:47:14 +0200

    DOM Traversing

--- apr/28-04/index.js
+++ apr/28-04/index.js
@@ -1,9 +1,24 @@
 // Traversing
 //  Node vs Element Nodes
 
+const ele = document.querySelector("#second").previousElementSibling;
+console.log(ele);
+const ele2 = document.querySelector("body").previousSibling;
+console.log(ele2);
 // firstChild
-
+const mainText = document.querySelector("#text").firstElementChild;
+console.log(mainText.textContent);
 // nodeName, nodeType, id, innerText
+let text = document.querySelector("span");
+let closetEle = text.closest(".cool");
+console.log(closetEle);
+
+let names = document.querySelectorAll("li");
+names.forEach((name) => {
+  console.log(name);
+  if (name.matches(".teacher"))
+    console.log(`${name.textContent} is a teacher for FBW 48-1`);
+});
 
 // textContent gets the content of all elements, including <script> and <style> elements. In contrast.
 // innerText only shows “human-readable” elements.
