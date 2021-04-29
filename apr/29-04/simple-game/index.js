0ea949872fb4b6b8410f614e83b689bf49f0e486
hnsreeny <hnsreeny@gmail.com>
2021-04-29 13:23:21 +0200

    Sipmle Game

--- apr/29-04/simple-game/index.js
+++ apr/29-04/simple-game/index.js
@@ -1,7 +1,7 @@
 // What is Q key code? so here all the keys code you need to know
 // https://keycode.info/
 let hStandPoint = 0;
-let vStandPont = 0;
+let vStandPoint = 0;
 function goUp() {
   hStandPoint -= 10;
   mini.style.top = hStandPoint + "px";
@@ -18,3 +18,14 @@ function goLeft() {
   vStandPoint -= 10;
   mini.style.left = vStandPoint + "px";
 }
+
+function move(e) {
+  console.log(e);
+  if (e.code == "KeyW") goUp();
+  if (e.code == "KeyD") goRight();
+  if (e.code == "KeyA") goLeft();
+  if (e.code == "KeyY") goDown();
+}
+const mini = document.querySelector(".mini");
+const body = document.querySelector("body");
+body.addEventListener("keypress", move);
