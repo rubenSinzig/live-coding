d5029b894e3f3157f8b40c67c42149416b3f31d7
hnsreeny <hnsreeny@gmail.com>
2021-05-04 08:20:41 +0200

    More DOM Events

--- may/04-05/index.js
+++ may/04-05/index.js
@@ -1,1 +1,43 @@
 // DOM tree
+function redBg() {
+  this.style.backgroundColor = "red";
+}
+function whiteBg() {
+  this.style.backgroundColor = "white";
+}
+function newUser() {
+  result.innerHTML += "Hi this is me <br>";
+}
+
+function windowLoad() {
+  //setInterval(newUser, 1000);
+  // alert("Hi new person");
+}
+
+const stop = () => {
+  header.removeEventListener("mouseover", redBg);
+
+  header.removeEventListener("mouseleave", whiteBg);
+};
+function changeColor(e) {
+  e.preventDefault();
+  form.style.backgroundColor = "orange";
+}
+
+let header = document.querySelector("h1");
+let result = document.querySelector(".result");
+header.addEventListener("mouseover", redBg);
+// header.removeEventListener("mouseover", function () {
+//     this.style.backgroundColor = "red";
+//   });
+header.addEventListener("mouseleave", whiteBg);
+
+window.addEventListener("load", windowLoad);
+
+let form = document.querySelector("form");
+form.addEventListener("submit", () => {
+  alert("Thanks for your feedback");
+});
+form.addEventListener("reset", () => {
+  alert("Your Input has been cleared out");
+});
