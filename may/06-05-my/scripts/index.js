342ff763607ed6cd1efa29ec2dbe0f196ec54c03
rubenSinzig <ruben.sinzig@posteo.de>
2021-05-06 15:30:16 +0200

    JS -> function & event -> ToDos API

--- may/06-05-my/scripts/index.js
+++ may/06-05-my/scripts/index.js
@@ -1,3 +1,30 @@
 // Year for Copyright
 const year = new Date().getFullYear();
 document.querySelector(".year").innerHTML = year;
+
+// event for click
+const todos = document.querySelector(".todos");
+const comments = document.querySelector(".comments");
+todos.addEventListener("click", fetchTodos);
+comments.addEventListener(".click", fetchComments);
+
+// function for todos
+function fetchTodos() {
+  fetch("https://jsonplaceholder.typicode.com/todos")
+    .then((response) => response.json())
+    .then((data) => {
+      let output = "<h3>Todos API response</h3> ";
+      data.forEach((todo) => {
+        let { id, title, completed, userId } = todo;
+        output += `
+        <div id="${id}" class="api-class">
+        <h4>Your User-ID is: ${userId}</h4>
+        <p>ToDo: ${title}</p>
+        <p>Completed: ${completed}</p>
+        </div>
+        `;
+      });
+      document.querySelector(".resultTodos").innerHTML = output;
+    })
+    .catch((err) => console.log(err));
+}
