fd3c790a074a8b4960a61a26343a1d7a73b32a99
hnsreeny <hnsreeny@gmail.com>
2021-05-06 08:59:36 +0200

    Promise in js

---
+++ may/06-05/index.js
@@ -0,0 +1,10 @@
+// Blocking vs Non-Blocking code
+// Blocking code
+// blacking code(loops, console.log, loading files )
+// Non-blocking code
+
+// Promises
+// Promises are very similar to callbacks but they are a little bit clean on writing
+// Promises are the best solution if you want to do something that takes a long time to be done and you don't want your app to wait for it to be done, so it will work on the background, Like downloading an image or a video from another server
+
+// Promisify setTimeout
