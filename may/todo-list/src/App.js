8e123999d6285e7ffcfc1e9dcb74f5eaca49962b
hnsreeny <hnsreeny@gmail.com>
2021-05-26 14:36:31 +0200

    React ToDo-list

--- may/todo-list/src/App.js
+++ may/todo-list/src/App.js
@@ -1,28 +1,16 @@
 import { useState } from "react";
 
 function App() {
-  const [userInput, setUserInput] = useState("");
-  function handleChange(e) {
-    setUserInput(e.target.value.trim());
-  }
-  function handleSubmit(e) {
-    e.preventDefault();
-    // more code
+  const [clicks, setClicks] = useState(0);
+
+  function increment() {
+    setClicks((prevState) => prevState + 1);
   }
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          className="userInput"
-          type="text"
-          value={userInput}
-          onChange={handleChange}
-        />
-        <input type="text" />
-        <input type="submit" value="Add" />
-      </form>
-      {userInput}
-      <h2>{userInput}</h2>
+      You click {clicks} times!
+      <br />
+      <button onClick={increment}>Click me 😎</button>
     </div>
   );
 }
