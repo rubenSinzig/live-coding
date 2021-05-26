cf041932d91166ec0c11e75a9bb57efabdc432e3
hnsreeny <hnsreeny@gmail.com>
2021-05-26 10:52:09 +0200

    React props

--- may/26-05/src/App.js
+++ may/26-05/src/App.js
@@ -1,10 +1,12 @@
 import { Text, Hallo } from "./components/Text";
 let name = "Hadi";
+const names = ["Zain", "Olga", "Nancy"];
+const listItems = names.map((name, i) => <li key={i}>{name}</li>);
 function App() {
   return (
     <div>
       <h1>Hi I am React</h1>
-      <Hallo userName={name} />
+      <Hallo userName={name} age="31" namesArr={names} />
       <Text />
       <Text />
       <Text />
@@ -15,6 +17,15 @@ function App() {
       <Text />
       <Text />
       <Text />
+      <ul>{listItems}</ul>
+
+      {
+        //   <ul>
+        //names.map((name, i) => (
+        //   <li key={i}>{name}</li>
+        // ))
+        //  </ul>
+      }
     </div>
   );
 }
