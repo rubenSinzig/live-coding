2543910b2f2ec03b2e00e26987a9f61d089c89be
hnsreeny <hnsreeny@gmail.com>
2021-05-31 09:28:12 +0200

    Functions in React

--- may/31-05/src/App.js
+++ may/31-05/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
-
+// import { add } from "./helpers";
+// import List from "./components/List";
 function App() {
-  return <React.Fragment></React.Fragment>;
+  // function print() {
+  //   return "Hey I am inner function";
+  // }
+  //const doSomething = () => 3 + 44 / 23;
+  return (
+    <React.Fragment>
+      {
+        //{doSomething()}
+        //   <List print={print} />
+        //   {add(22, 3)}
+      }
+    </React.Fragment>
+  );
 }
 
 export default App;
