40911f876262a089592e504abaa43233cad8df1b
hnsreeny <hnsreeny@gmail.com>
2021-05-31 09:36:31 +0200

    Functions in React

--- may/31-05/src/App.js
+++ may/31-05/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 // import { add } from "./helpers";
 // import List from "./components/List";
+import Data from "./data.json";
+console.log(Data);
+const users = Data.map((obj) => <h4 key={obj.id}>{obj.name}</h4>);
 function App() {
   // function print() {
   //   return "Hey I am inner function";
@@ -13,6 +16,7 @@ function App() {
         //   <List print={print} />
         //   {add(22, 3)}
       }
+      {users}
     </React.Fragment>
   );
 }
