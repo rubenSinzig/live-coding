2a1b1910fe457d5657ba1e5da576b8fa18b728b5
hnsreeny <hnsreeny@gmail.com>
2021-05-31 10:35:15 +0200

    Facebook not the real one

--- may/31-05/src/App.js
+++ may/31-05/src/App.js
@@ -4,20 +4,50 @@ import React from "react";
 import Data from "./data.json";
 
 console.log(Data);
-const users = Data.map((obj) => <h4 key={obj.id}>{obj.name}</h4>);
+
 function App() {
   // function print() {
   //   return "Hey I am inner function";
   // }
   //const doSomething = () => 3 + 44 / 23;
+  //const users = Data.map((obj) => <h4 key={obj.id}>{obj.name}</h4>);
+
+  const posts = Data.map((obj) => {
+    const {
+      id,
+      owner,
+      avatar,
+      title,
+      text,
+      likes,
+      comments,
+      share,
+      createdAt,
+    } = obj;
+    return (
+      <div className="posts" key={id}>
+        <h2>
+          {avatar} {owner} <span>{title}</span>
+          <span>{createdAt}</span>
+        </h2>
+        <h4>{text}</h4>
+        <div>
+          <span>👍🏻{likes}</span>
+          <span>💬{comments}</span>
+          <span>📤{share}</span>
+        </div>
+      </div>
+    );
+  });
   return (
     <React.Fragment>
       {
         //{doSomething()}
-        //   <List print={print} />
+        //   <List print={print}  name="Hadi" age="31"/>
         //   {add(22, 3)}
+        //   {users}
       }
-      {users}
+      {posts}
     </React.Fragment>
   );
 }
