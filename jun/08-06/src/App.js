ce6941c0daa0f39a992203348e7bbd39b7fd2440
hnsreeny <hnsreeny@gmail.com>
2021-06-08 09:48:22 +0200

    Axios

--- jun/08-06/src/App.js
+++ jun/08-06/src/App.js
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 const App = () => {
-  return <React.Fragment>Hi, I am React App</React.Fragment>;
+  const [userInput, setUserInput] = useState("");
+  function changeHandle(e) {
+    setUserInput(e.target.value);
+  }
+  function submitHandle(e) {
+    e.preventDefault();
+  }
+  return (
+    <React.Fragment>
+      <form onSubmit={submitHandle}>
+        <input
+          type="text"
+          value={userInput}
+          onChange={changeHandle}
+          placeholder="Write a country name"
+        />
+        <button type="submit">Search</button>
+      </form>
+    </React.Fragment>
+  );
 };
 
 export default App;
