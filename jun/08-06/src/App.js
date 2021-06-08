539bcc58d72ddd4ec71c03ed9a6ecf85c352e8b1
hnsreeny <hnsreeny@gmail.com>
2021-06-08 10:10:18 +0200

    Axios

--- jun/08-06/src/App.js
+++ jun/08-06/src/App.js
@@ -1,12 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Loading from "./components/Loading";
 const App = () => {
   const [userInput, setUserInput] = useState("");
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
   function changeHandle(e) {
     setUserInput(e.target.value);
   }
   function submitHandle(e) {
     e.preventDefault();
+    let textToUrl = encodeURIComponent(userInput);
+    console.log(textToUrl);
+    let endPoint = `https://restcountries.eu/rest/v2/name/${textToUrl}`;
   }
+  if (loading) return <Loading />;
+
   return (
     <React.Fragment>
       <form onSubmit={submitHandle}>
--- jun/08-06/src/components/Loading.js
+++ jun/08-06/src/components/Loading.js
@@ -4,6 +4,8 @@ const Loading = () => {
   return (
     <React.Fragment>
       <h4>🕸</h4>
+      <h6>Loading ...</h6>
     </React.Fragment>
   );
 };
+export default Loading;
