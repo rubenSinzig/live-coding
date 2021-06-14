00ca419f147c2d8e4eb2d52d185e542d2ff831b9
hnsreeny <hnsreeny@gmail.com>
2021-06-14 08:56:18 +0200

    Random small things

---
+++ jun/14-06/src/App.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+//import Data from "./data.json";
+//import ProductList from "./components/ProductList";
+
+const App = () => {
+  return <React.Fragment></React.Fragment>;
+};
+
+export default App;
---
+++ jun/14-06/src/components/ProductList.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+//import ProductItem from "./ProductItem";
+
+const ProductList = () => {
+  return <React.Fragment></React.Fragment>;
+};
+export default ProductList;
