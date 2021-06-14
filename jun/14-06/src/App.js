08732e4a629325b836a4d68c80c6efa98af08c1b
hnsreeny <hnsreeny@gmail.com>
2021-06-14 10:59:35 +0200

    Random small things

--- jun/14-06/src/App.js
+++ jun/14-06/src/App.js
@@ -13,12 +13,24 @@ import { WiAlien, WiNightAltThunderstorm } from "react-icons/wi";
 const App = () => {
   const [data, setData] = useState(Data);
   const [userInput, setUserInput] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
 
   const changeHandle = (e) => {
     setUserInput(e.target.value);
   };
+  const lookUp = () => {
+    const userText = userInput.toLocaleLowerCase().trim();
+    const userTextLength = userText.length;
+    // const searchText = userText ? userText : "";
+    let newArr = data.filter((item) => {
+      const slicedProductName = item.productName.slice(0, userTextLength);
+      return slicedProductName === userText;
+    });
+    setFilteredData(newArr);
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
+    lookUp();
   };
   return (
     <React.Fragment>
@@ -36,7 +48,7 @@ const App = () => {
         <input type="text" onChange={changeHandle} value={userInput} />
         <input type="submit" value="filter" />
       </form>
-      <ProductList data={data} />
+      <ProductList data={userInput ? filteredData : data} />
     </React.Fragment>
   );
 };
--- jun/14-06/src/components/ProductList.js
+++ jun/14-06/src/components/ProductList.js
@@ -4,17 +4,20 @@ import ProductItem from "./ProductItem";
 const ProductList = ({ data }) => {
   const [cart, setCart] = useState([]);
   const [totalBill, setTotalBill] = useState(0);
-
-  const items = data.map((item, i) => <ProductItem key={i} info={item} />);
   const addToCart = (item) => {
     setCart((prevStat) => [...prevStat, item]);
+    console.log(cart);
   };
+  const items = data.map((item, i) => (
+    <ProductItem key={i} info={item} addToCart={addToCart} />
+  ));
+
   const bill = () =>
     setTotalBill(cart.reduce((acc, cur) => acc + cur.price, 0));
   return (
     <React.Fragment>
       <div>
-        <button onClick={bill}>Total bill</button> <h6>{totalBill}</h6>
+        <button onClick={bill}>Total bill</button> <h6>{totalBill}€</h6>
       </div>
       <ul>{items}</ul>
     </React.Fragment>
