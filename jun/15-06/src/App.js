95a20ee8dcff8dfe482d72d34050757789fa9343
hnsreeny <hnsreeny@gmail.com>
2021-06-15 11:11:01 +0200

    react-router-dom

--- jun/15-06/src/App.js
+++ jun/15-06/src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Data from "./data.json";
-// import Product from "./components/Product";
-// import ProductInfo from "./components/ProductInfo";
+import Products from "./components/Products";
+import ProductInfo from "./components/ProductInfo";
 import About from "./components/About";
 import Menu from "./components/Menu";
 import Home from "./components/Home";
@@ -15,9 +15,29 @@ const App = () => {
       <Menu />
       <h1 className="jumbotron">Online Store</h1>
       <Switch>
+        {
+          // <Route path={process.env.PUBLIC_URL + "/"} exact component={Home} />
+        }
         <Route path="/" exact component={Home} />
+        <Route path="/about" exact component={About} />
+        <Route
+          path="/product"
+          exact
+          component={() => <Products data={Data} />}
+        />
+        <Route
+          path="/product/:id"
+          component={({ match }) => (
+            <ProductInfo data={Data} id={match.params.id} />
+          )}
+        />
+        {
+          //  <Route  path="/products"
+          // exact >
+          // <Products data={Data} />
+          // </Route>
+        }
         <Route path="/soon" component={Soon} />
-        <Route path="/about" component={About} />
       </Switch>
     </Router>
   );
