cff9def29dd5636bb46f2c4e6b2c85e8ce7bf975
hnsreeny <hnsreeny@gmail.com>
2021-06-21 10:19:53 +0200

    UseRef

--- jun/21-06/src/App.js
+++ jun/21-06/src/App.js
@@ -1,22 +1,26 @@
-import React, { useRef } from "react";
+import React, { useRef, Fragment } from "react";
 
 function App() {
+  const [counter, setCounter] = React.useState(0);
   const userTextInput = useRef(null);
+  const count = useRef(0);
+
+  const changeBg = () => (userTextInput.current.style.backgroundColor = "red");
   const getMe = () => {
     console.log(userTextInput);
     userTextInput.current.focus();
   };
-  const count = useRef(0);
-  const changeBg = () => (userTextInput.current.style.backgroundColor = "red");
   return (
-    <React.Fragment>
+    <Fragment>
       <input type="text" ref={userTextInput} />
       <button onClick={getMe}>Get me focused</button>
       <button onClick={changeBg}>Change Bg</button>
-
+      <h6>Counter with Ref, No Re-render</h6>
       <button onClick={() => count.current++}>{count.current}</button>
       {console.log(count.current)}
-    </React.Fragment>
+      <h6>Counter with State</h6>
+      <button onClick={() => setCounter(counter + 1)}>{counter}</button>
+    </Fragment>
   );
 }
 
