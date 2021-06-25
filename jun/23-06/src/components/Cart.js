6b6390b302bf287efc89d38d9f363c08f7fb4883
hnsreeny <hnsreeny@gmail.com>
2021-06-25 08:47:30 +0200

    online store with context

--- jun/23-06/src/components/Cart.js
+++ jun/23-06/src/components/Cart.js
@@ -5,7 +5,7 @@ const Cart = () => {
   // Not finished yet :)
   // This will be your task
   const contextObj = useContext(StoreContext);
-  const items = contextObj.Store.cart.map((item) => (
+  const items = contextObj.store.cart.map((item) => (
     <h3>{item.productName}</h3>
   ));
   return <ul>{items}</ul>;
