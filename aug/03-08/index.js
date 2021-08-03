87f5d72c7ad908e053b97286834146def3f980f0
hnsreeny <hnsreeny@gmail.com>
2021-08-03 09:37:58 +0200

    File sys && args

--- aug/03-08/index.js
+++ aug/03-08/index.js
@@ -3,16 +3,40 @@ const fs = require("fs");
 // writeFile will create or over write
 
 // appendFile will create file
+fs.appendFile("newText.txt", "I am text from NodeJs 😎 \n", (err) => {
+  if (err) throw err;
+  console.log("File is done 👍🏻");
+});
 
 // read File
+fs.readFile("./longText.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
 
 // read file Sync
+const data = fs.readFileSync("./longText.txt");
+console.log(data.toString());
+console.log("reading sync Done ✅");
 
 // rename file
+// fs.rename("./hello.txt", "./hey.txt", (err) => {
+//   if (err) throw err;
+//   console.log("Rename done ✅");
+// });
 
 // delete file
+// const filePathName = "./something.txt";
+// fs.unlink(filePathName, (err) => {
+//   if (err) throw err;
+//   console.log("File deleted, done ✅");
+// });
 
 // copy file
+fs.copyFile("./original.txt", "./copyFromOG.txt", (err) => {
+  if (err) throw err;
+  console.log("Copy file, Done ✅");
+});
 
 // open file
 // fs flags
