01acdaf49d31ee81cd48b394097b6d786511fcc3
rubenSinzig <ruben.sinzig@posteo.de>
2021-08-03 11:00:14 +0200

    live-coding

--- aug/03-08/index.js
+++ aug/03-08/index.js
@@ -48,3 +48,39 @@ fs.open("newText.txt", "w", (err, data) => {
   if (err) throw err;
   console.log("File is opened, Done ✅");
 });
+
+// writeFile will create or over write testFileOne.txt
+fs.writeFileSync("./testFileOne.txt", "This is the Test File One \n", (err) => {
+  if (err) throw err;
+  console.log("writeFile is done ✅");
+});
+
+// appendFile -> will create testFileOne and add new text to it without overwriting
+fs.appendFile(
+  "./testFileOne.txt",
+  "New line on every node index.js \n",
+  (err) => {
+    if (err) throw err;
+    console.log("New Line inside testFileOne is create ✅");
+  }
+);
+
+// readFile will read testFileOne.txt
+fs.readFile("./testFileOne.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
+
+// copyFile -> makes a copy of testFileOne
+fs.copyFile("./testFileOne.txt", "testFileTwo.txt", (err) => {
+  if (err) throw err;
+  console.log("Copy from testFileOne -> testFileTwo is done ✅");
+});
+
+// open file
+// fs flags
+// https://nodejs.org/docs/latest-v12.x/api/fs.html#fs_file_system_flags
+fs.open("./testFileTwo", "w", (err) => {
+  if (err) throw err;
+  console.log("testFileTwo is opened ✅");
+});
