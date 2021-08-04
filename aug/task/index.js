4336dd952f9664a27c53148ef3c07b9635035c28
hnsreeny <hnsreeny@gmail.com>
2021-08-04 11:37:00 +0200

    Capitalise character task

--- aug/task/index.js
+++ aug/task/index.js
@@ -8,3 +8,12 @@ if (args.includes("--help")) {
 if (args.includes("-v")) {
   showVersion();
 }
+
+prepareString = (str) => {
+  let newArray = str.map((word) => {
+    let fixedWord = word.trim();
+    return fixedWord.charAt(0).toUpperCase() + fixedWord.slice(1).toLowerCase();
+  });
+  console.log(newArray.join(" ").replace(/[^a-zA-Z ]/g, ""));
+};
+prepareString(args);
--- aug/task/message.js
+++ aug/task/message.js
@@ -1,9 +1,14 @@
 exports.showHelp = () => {
-  const helpText = `
+  const text = `
       How to use this program:
       1. When you input a new string with more than 1 space the program will sanitize it for you.
       2. If you typed the name of a city without capitalization the program will capitalize it for you.
       3. If you included a Capitalized letter in the wrong place of your string the program will sanitize it for you.
       `;
-  console.log(helpText);
+  console.log(text);
+};
+
+exports.showVersion = () => {
+  const text = `1.0.0v`;
+  console.log(text);
 };
