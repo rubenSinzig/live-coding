a015cfa2e9236614908259fce2f5f73f4d52db73
hnsreeny <hnsreeny@gmail.com>
2021-08-04 09:33:13 +0200

    Axios in NodeJs

---
+++ aug/04-08/src/lib/weather-client.js
@@ -0,0 +1,9 @@
+const axios = require("axios");
+const url = require("url");
+
+class OpenWeatherClient {
+  constructor(apiKey) {
+    this.apiKey = apiKey;
+    this.baseUrl = "http://api.openweathermap.org/data/2.5/";
+  }
+}
