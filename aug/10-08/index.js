d1cf53e1489a793acae9785c12c898926f238a69
hnsreeny <hnsreeny@gmail.com>
2021-08-10 08:37:13 +0200

    ExpressJs

---
+++ aug/10-08/index.js
@@ -0,0 +1,9 @@
+// server
+// https://digitalthinkerhelp.com/what-is-client-server-architecture-diagram-types-examples-components/
+// request response cycle
+// https://cascadingmedia.com/insites/2015/03/http-2.html
+// The Hypertext Transfer Protocol (HTTP)
+// HTTP status
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+// HTTP Methods
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
