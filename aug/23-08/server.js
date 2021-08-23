85c002241f0b4a4320699533111c800c71ebde1f
hnsreeny <hnsreeny@gmail.com>
2021-08-23 09:17:52 +0200

    REST API

--- aug/23-08/server.js
+++ aug/23-08/server.js
@@ -20,11 +20,12 @@ To start new express project
 // Your DB is able to CRUD aka Creat Retrieve Update Delete
 
 // Your server is able to REST API (REpresentational State Transfer) aka server can do (create, delete, update, read)requests
+// sometimes you see RESTFull API well thats exactly the same but also abel to handle
 require("dotenv").config();
 const http = require("http");
 const app = require("./app");
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 server.listen(PORT, () => {
