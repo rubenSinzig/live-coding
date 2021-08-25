dc2082cac89ea52e9be09ad74773ba0ebfe3ca88
hnsreeny <hnsreeny@gmail.com>
2021-08-25 11:15:59 +0200

    REST API

--- aug/25-08/controllers/employeeController.js
+++ aug/25-08/controllers/employeeController.js
@@ -24,6 +24,8 @@ const getEmployee = async (req, res, next) => {
 // Get one add
 const getAdd = async (req, res, next) => {
   // find will get all or with criteria wil get some
+  // with limit you can define how many should find get you
+  // employee = await EmployeesData.find({ add: req.params.add }).limit(70);
   let employee;
   try {
     employee = await EmployeesData.find({ add: req.params.add });
@@ -41,23 +43,23 @@ const getAdd = async (req, res, next) => {
 
 // View all Employees
 
-const getAllEmployees = async (error, req, res, next) => {
+const getAllEmployee = async (req, res) => {
   try {
+    // const employees = await EmployeesData.find().select("name age");
+
     const employees = await EmployeesData.find();
-    // 200 OK
-    //console.log(employees);
+    // 200 for Successful Ok
+    // console.log(employees);
     res.status(200).json(
       employees.map((employee) => {
-        const { _id, name, age, add, employeeAddedDate } = employee;
         return {
-          Id: _id,
-          Name: name,
-          Age: age,
-          Address: add,
-          employeeAddedDate: employeeAddedDate,
+          employeeId: employee._id,
+          employeeName: employee.name,
+          age: employee.age,
+          employeeAddedDate: employee.employeeAddedDate,
           request: {
             type: "GET",
-            url: `http://localhost:5000/employees/${name}`,
+            url: `http://localhost:5000/employees/${employee.name}`,
           },
         };
       })
@@ -67,11 +69,12 @@ const getAllEmployees = async (error, req, res, next) => {
     res.status(500).json({ message: err.message });
   }
 };
+// Get one Employee upon criteria
 const getOneEmployee = async (req, res) => {
-  // 200 Ok
+  // 200 Successful Ok
   res.status(200).json(res.employee);
 };
-
+// PATCH one Employee upon criteria
 const updateOneEmployee = async (req, res) => {
   const { name, age, add } = req.body;
   if (name) {
@@ -88,14 +91,51 @@ const updateOneEmployee = async (req, res) => {
     await res.employee.save();
     res.status(200).json({ message: "Employee updated", data: res.employee });
   } catch (err) {
-    // 400 for bad req
+    // 400 for bad request
     res.status(400).json({ message: err.message });
   }
 };
+// Add new Employee
+const addNewEmployee = async (req, res) => {
+  const employee = new EmployeesData({
+    name: req.body.name,
+    age: req.body.age,
+    add: req.body.add,
+  });
+  try {
+    // save
+    const newEmployee = await employee.save();
+    // 201 for Successful Created
+    res.status(201).json(newEmployee);
+  } catch (err) {
+    // 400 for Bad request
+    res.status(400).json({
+      message: err.message,
+    });
+  }
+};
+
+// DELETE one Employee upon criteria
+const deleteOneEmployee = async (req, res) => {
+  try {
+    // remove
+    await res.employee.remove();
+    // 200 for Successful OK
+    res.status(200).json({ message: "Employee Deleted" });
+  } catch (err) {
+    // 400 for Internal server error
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
 module.exports = {
   getEmployee,
   getAdd,
-  getAllEmployees,
+  getAllEmployee,
   getOneEmployee,
   updateOneEmployee,
+  addNewEmployee,
+  deleteOneEmployee,
 };
