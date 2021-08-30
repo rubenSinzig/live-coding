2b7c2e67417c99bdd96451dd245897c459cbbfd6
hnsreeny <hnsreeny@gmail.com>
2021-08-30 11:07:03 +0200

    Fake Data

--- aug/30-08/app.js
+++ aug/30-08/app.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 const faker = require("faker");
 // Get you the
 const path = require("path");
-//const fakeModel = require("./model/user");
+const fakeModel = require("./model/user");
 mongoose
   .connect(process.env.DB_URL, {
     useNewUrlParser: true,
@@ -24,8 +24,33 @@ app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "views"));
 
 app.get("/", (req, res) => {
-  res.render("home", { message: "Test" });
+  // res.render("home", { message: "Test" });
+  fakeModel.find((err, data) => {
+    if (err) {
+      console.log(err);
+    } else if (data) {
+      // res.render("home",{data:data})
+      res.render("home", { data });
+    } else {
+      res.render("home", { data: {} });
+    }
+  });
 });
-app.post("/", (req, res) => {});
+app.post("/", (req, res) => {
+  for (let i = 0; i < 10; i++) {
+    const fakeData = new fakeModel({
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      city: faker.address.city(),
+      imageUrl: faker.image.imageUrl(),
+    });
+    try {
+      fakeData.save();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  res.redirect("/");
+});
 
 module.exports = app;
