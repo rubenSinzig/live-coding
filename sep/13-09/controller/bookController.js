a810eb045044c4f10ca4c44b87d0e2cc1cf17e64
hnsreeny <hnsreeny@gmail.com>
2021-09-13 09:56:28 +0200

    Relations part 2 population and ref

--- sep/13-09/controller/bookController.js
+++ sep/13-09/controller/bookController.js
@@ -1,3 +1,27 @@
+const { Author, Book } = require("../model/authorModel");
+const mongoose = require("mongoose");
 const booksControllers = {};
+
+// GET all authors
+booksControllers.getAllAuthors = async (req, res) => {
+  try {
+    const authors = await Author.find();
+    res.status(200).json(authors);
+  } catch (err) {
+    res.status(err.message).json({ message: err.message });
+  }
+};
+// GET all books
+booksControllers.getAllBooks = async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.status(200).json(books);
+  } catch (err) {
+    res.status(err.message).json({ message: err.message });
+  }
+};
+// POST author
+
+// POST book
 
 module.exports = booksControllers;
