b9351bd6283c35d8246f58c0c53544740160899f
hnsreeny <hnsreeny@gmail.com>
2021-09-27 09:50:02 +0200

    Full-stack App

--- sep/27-09/client/src/App.js
+++ sep/27-09/client/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+//import AddForm from './'
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const getData = () => {
+    fetch("/users")
+      .then((result) => result.json())
+      .then((data) => setUsers(data));
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  const userArr = users.map((user, i) => <li key={i}>{user}</li>);
+  console.log(users);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>{userArr}</ul>
     </div>
   );
 }
