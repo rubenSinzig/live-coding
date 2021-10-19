214f5abd61ed0be692c2fe45c28f53b588521c53
hnsreeny <hnsreeny@gmail.com>
2021-10-19 10:27:22 +0200

    React-native App

--- react-native-app/app/screens/About.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Text } from "react-native";
-
-const About = () => <Text>About me or you :) </Text>;
-
-export default About;
--- react-native-app/app/screens/More.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Text } from "react-native";
-
-const More = () => <Text>More me or you :) </Text>;
-
-export default More;
